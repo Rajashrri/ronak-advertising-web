74b15920fa9b2eb5eacbbef0c22089ea68fd0b5e
Devendramali <devendramali01234@gmail.com>
2026-07-31 15:39:32 +0530

    new changes

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -9,9 +9,9 @@ const Layout = () => {
   return (
     <>
     <Header/>
-    <div className='webbody'>
+    {/* <div className='webbody'> */}
     <Outlet/>
-    </div>
+    {/* </div> */}
     <Footer/>
     </>
   )
