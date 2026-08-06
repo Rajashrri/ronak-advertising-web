20a3895b3d17f4056fe130da0edf56634f23e044
Devendramali <devendramali01234@gmail.com>
2026-08-06 15:37:25 +0530

    new chnages

--- src/component/home/HomeAbout.jsx
+++ src/component/home/HomeAbout.jsx
@@ -39,7 +39,7 @@ const HomeAbout = () => {
                     titleclass="text-black"
                     />
                     <p data-gsap>For over three decades, Ronak Advertising has helped brands become part of Navi Mumbai’s everyday landscape. We combine premium media locations, deep local understanding and dependable execution to create campaigns that are seen, remembered and acted upon.</p>
-                       <a data-gsap href="#!" className='btn2'>
+                       <a data-gsap href="/about" className='btn2'>
            Discover Our Story
             <svg width="13" height="11" viewBox="0 0 13 11" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M11.9766 5.96094L7.60156 10.3359C7.4375 10.5 7.21875 10.582 7 10.582C6.75391 10.582 6.53516 10.5 6.37109 10.3359C6.01562 10.0078 6.01562 9.43359 6.37109 9.10547L9.24219 6.20703H0.875C0.382812 6.20703 0 5.82422 0 5.33203C0 4.86719 0.382812 4.45703 0.875 4.45703H9.24219L6.37109 1.58594C6.01562 1.25781 6.01562 0.683594 6.37109 0.355469C6.69922 0 7.27344 0 7.60156 0.355469L11.9766 4.73047C12.332 5.05859 12.332 5.63281 11.9766 5.96094Z" fill="#1A1A1A"/>
--- src/component/home/WatchSomeVideo.jsx
+++ src/component/home/WatchSomeVideo.jsx
@@ -23,12 +23,10 @@ const WatchSomeVideo = () => {
                     />
                     <p className='mb-0' data-gsap>As Navi Mumbai grew, Ronak helped shape its outdoor media landscape. Today, our network connects brands with audiences across the city’s most important commercial, residential and transit corridors.</p>
                       
-                      <div className="input whatcv" data-gsap>
-                        <input type="text" placeholder='Search by area or location' />
-                        <button>
-                            Explore
-                        </button>
-                      </div>
+                      {/* <div className="input whatcv" data-gsap>
+                        <input type="text" placeholder='Search by area or location' /> */}
+                        <a href="/contact" class="btn ms-0">Contact Us<svg width="13" height="11" viewBox="0 0 13 11" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.9766 5.96094L7.60156 10.3359C7.4375 10.5 7.21875 10.582 7 10.582C6.75391 10.582 6.53516 10.5 6.37109 10.3359C6.01562 10.0078 6.01562 9.43359 6.37109 9.10547L9.24219 6.20703H0.875C0.382812 6.20703 0 5.82422 0 5.33203C0 4.86719 0.382812 4.45703 0.875 4.45703H9.24219L6.37109 1.58594C6.01562 1.25781 6.01562 0.683594 6.37109 0.355469C6.69922 0 7.27344 0 7.60156 0.355469L11.9766 4.73047C12.332 5.05859 12.332 5.63281 11.9766 5.96094Z" fill="white"></path></svg></a>
+                      {/* </div> */}
                     </div>
                 </div>
             </div>
