96282cec26c51f44c3f1c0d0996840f71c75fbc0
Devendramali <devendramali01234@gmail.com>
2026-08-27 18:05:08 +0530

    new

--- src/component/location/InquiryForm.jsx
+++ src/component/location/InquiryForm.jsx
@@ -184,6 +184,7 @@ const validate = () => {
                  <div className="col-lg-6">
                   <div className="input-box">
                        <div className="custom-select">
+                        <label htmlFor="">Media Type<span>*</span></label>
                               <select
                                 name="mediaType"
                                 value={formData.mediaType}
@@ -199,20 +200,20 @@ const validate = () => {
                                 <option value="Other">Other </option>
                               </select>
                                  <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                        >
-                          <path
-                            d="M6 9L12 15L18 9"
-                            stroke="#F5F5F5"
-                            stroke-width="1.5"
-                            stroke-linecap="square"
-                            stroke-linejoin="round"
-                          />
-                        </svg>
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                  >
+                                    <path
+                                      d="M6 9L12 15L18 9"
+                                      stroke="#F5F5F5"
+                                      stroke-width="1.5"
+                                      stroke-linecap="square"
+                                      stroke-linejoin="round"
+                                    />
+                                  </svg>
 
                               {errors.mediaType && (
                                 <div className="text-danger">
